0641a477015843401c722bbcb6c9982bda016fe4
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-11-28 17:03:40 -0500

    Website setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false,  // This line disables the Next.js logo in the bottom left corner
 };
 
 export default nextConfig;
